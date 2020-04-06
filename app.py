e9c75b6bfdd0d2dc18c486506d00070f7faae2ce
Clinton-Davis <clintongadavis@gmail.com>
2020-04-06 15:02:52 +0100

    added bug

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 import os
-from flask import Flask
+from flask import Flask, redirect
 
 app = Flask(__name__)
+messages = []
+
+def add_messages(username, message):
+    message.append("{}: {}".format(username, message))
 
 @app.route('/')
 def index():
@@ -10,11 +14,14 @@ def index():
 
 @app.route('/<username>')
 def user(username):
-    return "HI " + username
+    """ Display chat messages """
+    return "Welcome, {0} - {1}".format(username, messages)
 
 @app.route('/<username>/<message>')
 def send_message(username, message):
-    return "{0}: {1}".format(username, message)
+    """Create a new message and redirect back to chat page"""
+    add_messages(username, message)
+    return redirect('/' + username)
 
 
 
